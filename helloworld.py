6be16c186b7da5e6b491892e4004f8bcf2e95441
Nicolas Bichon <nbichon@gmail.com>
2021-06-07 16:16:35 +0200

    hello world program

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+##############################################
+# Program allowing to print "hello world"    #
+#                                            #
+# Edited by Nicolas Bichon on June 2nd 2021  #
+#                                            #
+##############################################
+
+print("hello world")
